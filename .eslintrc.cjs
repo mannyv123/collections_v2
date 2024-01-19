4f2c261ec17baf8dba19164bb921bc0165ca4964
Manjot Virdi <virdi_manjot@hotmail.com>
2024-01-19 00:55:13 -0800

    lint stage update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,31 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:react-hooks/recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "prettier",
-    "plugin:prettier/recommended",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "@typescript-eslint"],
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-  },
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: ["./tsconfig.json", "./tsconfig.node.json"],
-    extraFileExtensions: [".json"],
-    tsconfigRootDir: __dirname,
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+   root: true,
+   env: { browser: true, es2020: true },
+   extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended-type-checked",
+      "plugin:react-hooks/recommended",
+      "plugin:react/recommended",
+      "plugin:react/jsx-runtime",
+      "prettier",
+      "plugin:prettier/recommended",
+   ],
+   ignorePatterns: ["dist", ".eslintrc.cjs"],
+   parser: "@typescript-eslint/parser",
+   plugins: ["react-refresh", "@typescript-eslint"],
+   rules: {
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+   },
+   parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      project: ["./tsconfig.json", "./tsconfig.node.json"],
+      extraFileExtensions: [".json"],
+      tsconfigRootDir: __dirname,
+   },
+   settings: {
+      react: {
+         version: "detect",
+      },
+   },
 };
