ac54530e54b3590bb6406d7303d24f8d2695d419
Manjot Virdi <virdi_manjot@hotmail.com>
2024-02-06 20:11:36 -0800

    update breakpoint styling for nav

--- src/components/NavFeature/NavFeature.tsx
+++ src/components/NavFeature/NavFeature.tsx
@@ -1,11 +1,26 @@
-import ButtonComponent from "../ButtonComponent/ButtonComponent";
+import { useState } from "react";
 import "./NavFeature.scss";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 function NavFeature() {
+   const [menuOpen, setMenuOpen] = useState(false);
+
+   const handleMenu = () => {
+      setMenuOpen((prev) => !prev);
+   };
+
    return (
       <nav className='nav'>
-         <ButtonComponent variant='secondary' text='Login' />
-         <ButtonComponent variant='primary' text='Sign Up' />
+         <div className='nav__icon-container' onClick={handleMenu}>
+            {menuOpen ? <CloseIcon fontSize='large' /> : <MenuIcon fontSize='large' />}
+         </div>
+         <div
+            className={`nav__actions ${menuOpen ? "nav__actions--open" : "nav__actions--closed"}`}
+         >
+            <button className='nav__action-button'>Login</button>
+            <button className='nav__action-button nav__action-button--sign-up'>Sign Up</button>
+         </div>
       </nav>
    );
 }
