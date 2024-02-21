3e34ed524c836d6b54960ecafffa7ca5a9355df8
Manjot Virdi <virdi_manjot@hotmail.com>
2024-02-20 18:32:24 -0800

    update styling for preview component

--- src/components/PreviewFeature/PreviewFeature.tsx
+++ src/components/PreviewFeature/PreviewFeature.tsx
@@ -1,25 +1,30 @@
 import "./PreviewFeature.scss";
+import marker from "../../assets/icons/marker.png";
+import CollectionsIcon from "@mui/icons-material/Collections";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 
 function PreviewFeature() {
    return (
       <section className='preview'>
-         <div className='preview__details'>Collection Detail</div>
+         <div className='preview__info'>
+            <div className='preview__info-item preview__info-item--location'>
+               <img src={marker} alt='marker' className='preview__marker-icon' />
+               <h3 className='preview__title'>Istanbul, Turkey</h3>
+            </div>
+            <div className='preview__info-item'>
+               <div className='preview__info-item preview__info-item--collection-name'>
+                  <CollectionsIcon fontSize='medium' />
+                  <p>Honeymoon</p>
+               </div>
+               <div className='preview__info-item preview__info-item--user'>
+                  <PersonOutlineIcon fontSize='medium' />
+                  <p>mannyvirdi</p>
+               </div>
+            </div>
+         </div>
          <div className='preview__images'>
             <div className='preview__image'></div>
             <div className='preview__thumbnails'>
-               <div className='preview__thumbnail-card'></div>
-               <div className='preview__thumbnail-card'></div>
-               <div className='preview__thumbnail-card'></div>
-               <div className='preview__thumbnail-card'></div>
-               <div className='preview__thumbnail-card'></div>
-               <div className='preview__thumbnail-card'></div>
-               <div className='preview__thumbnail-card'></div>
-               <div className='preview__thumbnail-card'></div>
-               <div className='preview__thumbnail-card'></div>
-               <div className='preview__thumbnail-card'></div>
-               <div className='preview__thumbnail-card'></div>
-               <div className='preview__thumbnail-card'></div>
-               <div className='preview__thumbnail-card'></div>
                <div className='preview__thumbnail-card'></div>
                <div className='preview__thumbnail-card'></div>
                <div className='preview__thumbnail-card'></div>
